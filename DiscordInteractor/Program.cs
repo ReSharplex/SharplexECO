ec23ec4a32d703748eecb5efb65b623bc674b703
ReSharplex <166249324+ReSharplex@users.noreply.github.com>
2024-11-10 00:52:10 +0100

    initialized new project

--- DiscordInteractor/Program.cs
+++ DiscordInteractor/Program.cs
@@ -4,21 +4,5 @@ class Program
 {
     static void Main(string[] args)
     {
-                public List<Message> GetChannelMessages(long channel_id, int message_count = 100)
-                {
-                    var request = new RestRequest($"https://discord.com/api/v9/channels/{channel_id}/messages?limit={message_count}", Method.Get);
-        
-                    var response = Global.client.Execute(Global.AddHeader(request));
-        
-                    JArray message_array = JArray.Parse(response.Content.Replace("\"", "'"));
-        
-                    List<Message> message_list = new List<Message>();
-        
-                    foreach (JObject json in message_array)
-                    {
-                        message_list.Add(Global.ConvertJsonToMessage(json));
-                    }
-                    return message_list;
-                }
     }
 }
---
+++ SniffSharp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SniffSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
